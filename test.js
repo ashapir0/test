ace504326ae8f56b05fb6420e81f65ff9f19d066
Aaron Shapiro <ashapiro@glgroup.com>
2020-08-28 10:55:10 -0400

    go

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log(process.env);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json(process.env);
+});
+
+app.listen(process.env.PORT || 8080);
